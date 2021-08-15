4ea95201c65c393945bd052481063046f94d1a64
ctmphuongg <phuong.caothiminh.contact@gmail.com>
2021-08-15 20:45:01 +0700

    Hackathon-2021

---
+++ script/check-log-in.js
@@ -0,0 +1,40 @@
+var user
+
+firebase.auth().onAuthStateChanged(function(u) {
+    var login = document.querySelector('#login-nav')
+    var register = document.querySelector('#register-nav')
+    var logout = document.querySelector('#logout-nav')
+
+    if (u) {
+        user = u
+        logout.style.display = 'block'
+        login.style.display = 'none'
+        register.style.display = 'none'
+    } else {
+        user = u
+        logout.style.display = 'none'
+        login.style.display = 'block'
+        register.style.display = 'block'
+    }
+    console.log(u)
+})
+var manage = document.querySelector('#manage')
+manage.style.display = 'none'
+db.collection("users").onSnapshot(querySnapshot => {
+    querySnapshot.forEach(doc => {
+        data = doc.data()
+        if (user && user.email==data.email && data.account=='admin') {
+            manage.style.display = 'block'
+        }
+    })
+})
+
+
+document.querySelector('#logout-nav').onclick = function() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        window.location.assign("index.html")
+      }).catch((error) => {
+        // An error happened.
+      });
+}
